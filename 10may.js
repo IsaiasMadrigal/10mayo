33c15db638027eaf39b5c23d152fcd1904cc598d
Isaías Madrigal <l21121544@morelia.tecnm.mx>
2024-05-08 09:48:46 -0600

    carta dia madres

---
+++ 10may.js
@@ -0,0 +1,8 @@
+function abrirCarta(){
+    document.getElementById("izq").style.display = "block";
+    document.getElementById("musica").play();
+    document.getElementById("poema1").style.color = "black";
+    document.getElementById("poema2").style.color = "black";
+    document.getElementById("fotito").style.display = "block";
+    document.getElementById("exterior").style.display = "none";
+}
